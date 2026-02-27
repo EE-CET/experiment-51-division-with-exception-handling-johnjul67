76f6945bb79683623a170a842c79a34f6b24c525
John <tve24el033@cet.ac.in>
2026-02-27 07:32:04 +0530

    c1

--- src/DivisionHandling.java
+++ src/DivisionHandling.java
@@ -1,11 +1,22 @@
 import java.util.Scanner;
 
-public class DivisionHandling {
-    
-        // TODO: Read two integers a and b
-        
-        // TODO: Enclose the division in a try block
-        
-        // TODO: Catch ArithmeticException
-        
+public class DivisionHandling 
+{
+    public static void main(String[] args) 
+    {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        try {
+            int result = a / b;
+            System.out.println(result);
+        }
+        catch (ArithmeticException e) 
+        {
+            System.out.println("Divide by zero error");
+        }
+        sc.close();
+    }
 }
